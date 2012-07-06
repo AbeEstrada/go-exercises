5a3b4a1706c55138e165951b91791513909028d2
Abraham Estrada <abraham.estrada@gmail.com>
2012-07-05 22:38:21 -0600

    fizzbuzz and factorial

---
+++ factorial.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(f(6))
+}
+
+func f(n int) int {
+    if n <= 1 {
+        return 1
+    } 
+    return n*f(n-1)
+}
---
+++ fizzbuzz.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func fb(x int) {
+    f, b := "Fizz", "Buzz"
+
+    for i := 1; i <= x; i++ {
+        if i % 15 == 0 {
+            fmt.Println(f+b)
+        } else if i % 5 == 0 {
+            fmt.Println(b)
+        } else if i % 3 == 0 {
+            fmt.Println(f)
+        } else {
+            fmt.Println(i)
+        }
+    }
+}
+
+func main() {
+    fb(100)
+}
