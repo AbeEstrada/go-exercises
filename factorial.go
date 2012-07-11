b79cf38472a8dba79829425d3aeb1e203de7cf78
Abraham Estrada <abraham.estrada@gmail.com>
2012-07-10 23:28:19 -0600

    concurrency example and 'go fmt *'

--- factorial.go
+++ factorial.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println(f(6))
+	fmt.Println(f(6))
 }
 
 func f(n int) int {
-    if n <= 1 {
-        return 1
-    } 
-    return n*f(n-1)
+	if n <= 1 {
+		return 1
+	}
+	return n * f(n-1)
 }
--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,21 +3,21 @@ package main
 import "fmt"
 
 func fb(x int) {
-    f, b := "Fizz", "Buzz"
+	f, b := "Fizz", "Buzz"
 
-    for i := 1; i <= x; i++ {
-        if i % 15 == 0 {
-            fmt.Println(f+b)
-        } else if i % 5 == 0 {
-            fmt.Println(b)
-        } else if i % 3 == 0 {
-            fmt.Println(f)
-        } else {
-            fmt.Println(i)
-        }
-    }
+	for i := 1; i <= x; i++ {
+		if i%15 == 0 {
+			fmt.Println(f + b)
+		} else if i%5 == 0 {
+			fmt.Println(b)
+		} else if i%3 == 0 {
+			fmt.Println(f)
+		} else {
+			fmt.Println(i)
+		}
+	}
 }
 
 func main() {
-    fb(100)
+	fb(100)
 }
